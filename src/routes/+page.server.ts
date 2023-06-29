59cf4b35e6446ffdcb545d7298cb39d1919646b2
cherrybll17253 <zeekee52@gmail.com>
2023-06-29 22:59:28 +0900

    Co-authored-by: myrlagksruf <myrlagksruf@users.noreply.github.com>

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from "./$types"
+
+interface iPlaceHolder{
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export const load:PageServerLoad = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return {
+        placeholder:(await res.json()) as iPlaceHolder[]
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import type { PageServerData } from "./$types";
+    export let data:PageServerData;
+
+    interface iHuman{
+        name:string;
+        age:number;
+    }
+    
+    let arr:iHuman[] = [];
+
+    onMount(async() => {
+        const res = await fetch('/test.json');
+        const json = await res.json();
+        arr = json as iHuman[];
+    })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each data.placeholder as { title, body }}
+    <div>
+        <div>제목 : {title}</div>
+        <div>내용 : {body}</div>
+    </div>
+{/each}
