7329ca3b0b412a221ef782742ad3a28670410be8
cherrybll17253 <zeekee52@gmail.com>
2023-07-01 14:03:15 +0900

    JSON tests run
    Co-authored-by: myrlagksruf <myrlagksruf@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import IconButton from "@smui/icon-button";
     import type { PageServerData } from "./$types";
     export let data:PageServerData;
+
+    let randomSlogan = Math.floor(Math.random()*3 + 1)
 
     interface iHuman{
         name:string;
@@ -17,11 +20,37 @@
     })
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-{#each data.placeholder as { title, body }}
-    <div>
-        <div>제목 : {title}</div>
-        <div>내용 : {body}</div>
+{#if randomSlogan == 1}
+    <div class="title">
+        <h5 style="text-align:left;">UD<br>GA<br>J I</h5>
+        <h1>"Your dream vacation awaits - let us light the way."</h1>
+        <IconButton class="material-icons">search</IconButton>
     </div>
-{/each}
+{/if}
+{#if randomSlogan == 2}
+    <div class="title">
+        <h5 style="text-align:left;">UD<br>GA<br>J I</h5>
+        <h1>"Your dream vacation starts here."</h1>
+        <IconButton class="material-icons">search</IconButton>
+    </div>
+{/if}
+{#if randomSlogan == 3}
+    <div class="title">
+        <h5 style="text-align:left;">UD<br>GA<br>J I</h5>
+        <h1>"Escape, explore, and create unforgettable memories."</h1>
+        <IconButton class="material-icons">search</IconButton>
+    </div>
+{/if}
+
+<style>
+    *{
+        margin:0;
+        padding:0;
+    }
+    .title{
+        padding:20px;
+        text-align:center;
+        background-color: violet;
+    }
+
+</style>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import IconButton from "@smui/icon-button";
+
+    let clicked = 0;
+</script>
+
+<div style="display: flex; align-items: center;">
+    <IconButton class="material-icons" on:click={() => clicked++}
+        >build</IconButton
+    >
+</div>
+<div style="display: flex; align-items: center;">
+    <IconButton class="material-icons" on:click={() => clicked++} disabled
+        >search</IconButton
+    >&nbsp;(disabled)
+</div>
+<div style="display: flex; align-items: center;">
+    <IconButton class="material-icons" on:click={() => clicked++} ripple={false}
+        >add_shopping_cart</IconButton
+    >&nbsp;(no ripple)
+</div>
+
+<pre class="status">Clicked: {clicked}</pre>
