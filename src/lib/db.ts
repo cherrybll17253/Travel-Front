9233378d25444b334dec8478398bdfe6d3a0e7d6
cherrybll17253 <zeekee52@gmail.com>
2024-05-12 18:15:24 +0900

    switched data base

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-const url: string = 'mongodb+srv://anothercbt:jian0806@cluster0.m5doiuj.mongodb.net/?retryWrites=true&w=majority';
+const url: string = 'mongodb+srv://thegreatcbt:jian!0806@udgajicluster.eluwvwj.mongodb.net/';
 const client: MongoClient = new MongoClient(url);
 const dbName: string = 'DBtest';
 
